c5be4093cf3a85c2cd0a5c588098da683ce77dd5
takkyuuplayer <takkyuuplayer@gmail.com>
2017-05-08 19:28:26 +0900

    Add python 2.3.1

--- common/lib.py
+++ common/lib.py
@@ -9,5 +9,5 @@ from matplotlib import pyplot as plt
 
 def plt_show_alt(plt):
     plt.savefig("/tmp/output.png")
-    process = Popen(["/usr/local/sbin/imgcat", "/tmp/output.png"])
+    #process = Popen(["/usr/local/sbin/imgcat", "/tmp/output.png"])
     plt.clf()
