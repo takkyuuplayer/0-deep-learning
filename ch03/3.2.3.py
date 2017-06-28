90df3665b93dc39ede3ed0bebfe419e1c58a4158
takkyuuplayer <takkyuuplayer@gmail.com>
2017-06-28 15:25:16 +0900

    Refactor 3.2.3

--- ch03/3.2.3.py
+++ ch03/3.2.3.py
@@ -1,12 +1,13 @@
-from common import lib as l
+import sys, os
+sys.path.append(os.curdir)
 
+import common.lib as l
 import numpy as np
 
-def step_function(x):
-    return np.array(x > 0, dtype=np.int)
+from common.functions import step
 
 x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
+y = step(x)
 
 l.plt.plot(x, y)
 l.plt_show_alt(l.plt)
