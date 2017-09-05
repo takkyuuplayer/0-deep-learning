86141efaff3bce487f61a939af67aec6f663e215
takkyuuplayer <takkyuuplayer@gmail.com>
2017-09-06 06:46:30 +0900

    Add 4.4

---
+++ ch04/4.4.py
@@ -0,0 +1,13 @@
+import sys, os
+sys.path.append(os.curdir)
+
+import numpy as np
+
+from common.functions import numerical_gradient
+
+def function_2(x):
+    return np.sum(x ** 2)
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+print(numerical_gradient(function_2, np.array([3.0, 0.0])))
--- common/functions.py
+++ common/functions.py
@@ -20,3 +20,22 @@ def softmax(a):
     y = exp_a / sum_exp_a
 
     return y
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+
+        x[idx] = tmp_val
+
+    return grad
