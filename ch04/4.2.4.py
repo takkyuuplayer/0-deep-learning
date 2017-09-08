9b54f1e8f4ef62d574515a8254193d31d39daf04
takkyuuplayer <takkyuuplayer@gmail.com>
2017-09-08 10:40:01 +0900

    Add 4.4

--- ch04/4.2.4.py
+++ ch04/4.2.4.py
@@ -4,6 +4,8 @@ sys.path.append(os.curdir)
 from datasets.mnist import load_mnist
 import numpy as np
 
+from common.functions import cross_entropy_error
+
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
 print(x_train.shape)
@@ -15,16 +17,6 @@ batch_mask = np.random.choice(train_size, batch_size)
 
 x_batch = x_train[batch_mask]
 t_batch = t_train[batch_mask]
-
-def cross_entropy_error(y, t):
-    if y.ndim == 1:
-        t = t.reshape(1, t.size)
-        y = y.reshape(1, y.size)
-
-    delta = 1e-7
-    batch_size = y.shape[0]
-
-    return -np.sum(t * np.log( y + delta )) / batch_size
 
 
 def cross_entropy_error_for_label(y, t):
--- common/functions.py
+++ common/functions.py
@@ -21,7 +21,17 @@ def softmax(a):
 
     return y
 
-def numerical_gradient(f, x):
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    delta = 1e-7
+    batch_size = y.shape[0]
+
+    return -np.sum(t * np.log( y + delta )) / batch_size
+
+def numerical_gradient_1d(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
 
@@ -38,4 +48,24 @@ def numerical_gradient(f, x):
 
         x[idx] = tmp_val
 
+    return grad
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 値を元に戻す
+        it.iternext()   
+        
     return grad
