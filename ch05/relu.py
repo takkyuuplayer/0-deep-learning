0ae8e4e60b55a8825e31de26b37169090a8d05fe
takkyuuplayer <takkyuuplayer@gmail.com>
2017-11-04 21:00:34 -0400

    Add 5.7.3

--- ch05/relu.py
+++ ch05/relu.py
@@ -2,14 +2,14 @@ class Relu:
     def __init__(self):
         self.mask = None
 
-    def forward(self, x, y):
+    def forward(self, x):
         self.mask = ( x <= 0 )
         out = x.copy()
-        out[self.masx] = 0
+        out[self.mask] = 0
 
         return out
 
-    def backward(self, x):
+    def backward(self, dout):
         dout[self.mask] = 0
         dx = dout
 
